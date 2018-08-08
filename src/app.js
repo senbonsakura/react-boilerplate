973938c204f07dbfd41364acf2524a12cb741274
senbonsakura <overlord_cihat@yahoo.com>
2018-08-08 13:20:44 +0300

    Eslint configuration updated

--- src/app.js
+++ src/app.js
@@ -1,44 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import AppRouter, { history } from './routers/AppRouter'
-import configureStore from './store/configureStore'
-import { login, logout } from './actions/auth'
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
-import 'react-dates/lib/css/_datepicker.css'
-import { firebase } from './firebase/firebase'
-import LoadingPage from './components/LoadingPage'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { login, logout } from './actions/auth';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
-const store = configureStore()
+const store = configureStore();
 
 const jsx = (
   <Provider store={store}>
-    <AppRouter/>
+    <AppRouter />
   </Provider>
-)
+);
 
-let hasRendered = false
+let hasRendered = false;
 const renderApp = () => {
   if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('app'))
-    hasRendered = true
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
   }
-}
-ReactDOM.render(<LoadingPage />, document.getElementById('app'))
+};
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    store.dispatch(login(user.uid))
+    store.dispatch(login(user.uid));
     renderApp();
     if (history.location.pathname === '/') {
-      history.push('/dashboard')
+      history.push('/dashboard');
     }
   } else {
-    store.dispatch(logout())
-    renderApp()
-    history.push('/')
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
   }
-})
-
+});
